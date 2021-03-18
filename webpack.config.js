59c6f8ea1fd32a849b1149a0a9fbe87f5bb667da
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-03-18 22:16:12 +0200

    add plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
-}
+  entry: "./src/client/index.js",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      name: "./index.html",
+    }),
+  ],
+};
